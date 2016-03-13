7d018bc23456684fc673f96cd982f67f7331e2b5
Patrick Bay <monicanagent@users.noreply.github.com>
2016-03-12 21:36:23 -0500

    Updated "About" section.

--- main.js
+++ main.js
@@ -23415,7 +23415,7 @@ module.exports = React.createClass({
                         React.createElement(
                             "p",
                             null,
-                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            "CypherPoker is an open source, peer-to-peer Texas Hold'em poker game. It uses strong cryptography and smart contracts to eliminate the need for centralized, trusted third parties."
                         )
                     ),
                     React.createElement(
