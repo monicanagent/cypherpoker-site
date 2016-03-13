2353e886d8681e12133d085c302cc766d0d71596
Patrick Bay <monicanagent@users.noreply.github.com>
2016-03-12 22:45:54 -0500

    Testing footer license markup.

--- main.js
+++ main.js
@@ -23495,9 +23495,21 @@ module.exports = React.createClass({
                         React.createElement(
                             "p",
                             { className: "small" },
-                            {{__html: "<a rel='license' href='http://creativecommons.org/licenses/by/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by/4.0/88x31.png' /></a><br />This work is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by/4.0/'>Creative Commons Attribution 4.0 International License</a>."}}
-                        )
-						
+							React.createElement(
+								"a",
+								{rel:'license', href:'http://creativecommons.org/licenses/by/4.0/'},
+								React.createElement(
+									"img",
+									{alt: 'Creative Commons License', style:'border-width:0', src:'https://i.creativecommons.org/l/by/4.0/88x31.png'},
+									null
+								),		
+								React.createElement(
+									"span",
+									{},
+									"This work is licensed under a Creative Commons Attribution 4.0 International License."
+								)
+							)
+                        )						
                     )
                 )
             )
